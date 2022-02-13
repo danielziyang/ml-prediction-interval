2c499a454b2b30e20d68d6c86b24e232a270a4db
Daniel Zi Yang <dziyang95@gmail.com>
2022-02-13 15:10:59 -0700

    more tests

--- tests/data_prep/test_categorical.py
+++ tests/data_prep/test_categorical.py
@@ -15,3 +15,37 @@ def test_extract_title():
 
     result = extract_title(lowercased_df, col='string')
     assert result['title'].tolist() == ['mme', 'ms', 'mr', 'lady', 'major']
+
+@pytest.fixture 
+def dummy_df():
+    string_col = ['Futrelle, Mme. Jacques Heath (Lily May Peel)',
+                  'Johnston, Ms. Catherine Helen "Carrie"',
+                  'Sloper, Mr. William Thompson',
+                  'Ostby, Lady. Engelhart Cornelius',
+                  'Backstrom, Major. Karl Alfred (Maria Mathilda Gustafsson)']
+    df_dict = {'string': string_col}
+    df = pd.DataFrame(df_dict)
+    return df
+
+@pytest.fixture
+def lowercased_df():
+    string_col = ['futrelle, mme. jacques heath (lily may peel)',
+                  'johnston, ms. catherine helen "carrie"',
+                  'sloper, mr. william thompson',
+                  'ostby, lady. engelhart cornelius',
+                  'backstrom, major. karl alfred (maria mathilda gustafsson)']
+    df_dict = {'string': string_col}
+    df = pd.DataFrame(df_dict)
+    return df
+
+def test_lowercase_string():
+    assert lowercase_string('abc') == 'abc'
+    assert lowercase_string('ABC') == 'abc'
+    assert lowercase_string('Abc123') == 'abc123'
+
+
+def test_lowercase_column(dummy_df, lowercased_df):
+    actual_df = lowercase_column(dummy_df, col='string')
+    assert actual_df.equals(lowercased_df)
+
+
